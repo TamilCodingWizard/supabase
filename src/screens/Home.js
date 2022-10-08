d971ed60d26d78ec3e2fdef711fe7307d3f6254f
tamilCodingWizard <tamilcodingwizard@gmail.com>
2022-10-08 18:40:25 +0100

    Video9

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Grid, Typography } from "@mui/material";
 import StudentCard from "../components/Studentcard";
 import supabase from './../config/supabase';
+import Loader from "../components/Loader";
 
 const Home = () => {
   const [students, setStudents] = useState(null);
   const [fetchError,setFetchError] = useState(null)
+  const [isFetching,setIsFetching] = useState(false)
 
   const deleteStudent = (id) => {
     setStudents((prevStudents) => {
@@ -15,6 +17,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsFetching(true)
       const { data, error } = await supabase.from("Students").select();
 
       if (error) {
@@ -28,18 +31,23 @@ const Home = () => {
         setFetchError(null)
         console.log(data)
       }
+      setIsFetching(false)
 
     };
 
     fetchData();
   }, []);
 
+  if (isFetching) {
+    return <Loader/>
+  }
+
   return (
     <Grid container spacing={5}>
       {students &&
         students.map((student) => {
           return (
-            <Grid item lg={4} xl={4} key={student.id}>
+            <Grid item lg={4} xl={4} md={4} sm={6} xs={12} key={student.id}>
               <StudentCard student={student} deleteStudent={deleteStudent} />
             </Grid>
           );
